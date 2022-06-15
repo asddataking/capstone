d8652ec9893705d08aa22fc1968235a0b74a23d7
asddataking <asddataking@outlook.com>
2022-06-15 14:40:57 -0400

    Updated Color Scheme/Fixed Buttons/Fixed Blog other updates

--- config/myapp/views.py
+++ config/myapp/views.py
@@ -5,6 +5,9 @@ from .forms import SignupForm
 def index(request):
     return render(request, 'myapp/index.html')
 
+def price(request):
+    return render(request, 'myapp/pricing.html')
+
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
